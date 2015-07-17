80ed5bdbdea13a4782e5a78a7c23c37d6c5c213e
dierat <diedra.rater@gmail.com>
2015-07-16 21:17:52 -0700

    adds dummy data for character and enemy models

---
+++ data.js
@@ -0,0 +1,45 @@
+var characters = [
+  {
+    name: 'player1',
+    hp: 100,
+    mp: 100
+  },
+  {
+    name: 'player2',
+    hp: 100,
+    mp: 100
+  },
+  {
+    name: 'player3',
+    hp: 100,
+    mp: 100
+  },
+  {
+    name: 'player4',
+    hp: 100,
+    mp: 100
+  }
+];
+
+var enemies = [
+  {
+    name: 'enemy1',
+    hp: 100,
+    mp: 100
+  },
+  {
+    name: 'enemy2',
+    hp: 100,
+    mp: 100
+  },
+  {
+    name: 'enemy3',
+    hp: 100,
+    mp: 100
+  },
+  {
+    name: 'enemy4',
+    hp: 100,
+    mp: 100
+  }
+];
